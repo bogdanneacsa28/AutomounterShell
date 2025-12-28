ff11c7af211415330c4a04d94ecc757e0c11f285
Bogdan <bogdanandreineacsa42@gmail.com>
2025-12-28 19:24:25 +0200

    Add files via upload
    Am implementat intern comanda cd (afiseaza directorul curent si se modifica la comanda)

---
+++ amsh.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+path=$(pwd)
+x="amsh:$path> "
+echo -n "$x"
+read input
+
+while [ "$input" != "exit" -a "$input" != "bye" ]; do
+	eval "$input" 
+	command=$(echo $input | cut -d " " -f 1)
+    if [ "$command" = "cd" ]; then
+        path=$(pwd)
+        x="amsh:$path> "
+        echo -n "$x"
+    else
+        echo -n "$x"
+    fi
+	read input
+done
+
+
