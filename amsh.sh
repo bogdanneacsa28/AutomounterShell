8093bd07ad2cac2c31042bcd78032530c30cc71d
Cristian-Nastase <cristianemail@duck.com>
2026-01-07 21:02:16 +0200

    Am dat rebase de la commiturile de pe main

--- amsh.sh
+++ amsh.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
-x="amsh> "
+path=$(pwd)
+x="amsh:$path> "
 echo -n "$x"
 read input
 
 while [ "$input" != "exit" -a "$input" != "bye" ]; do
-	eval "$input"
-	
-	echo -n "$x"
+	eval "$input" 
+	command=$(echo $input | cut -d " " -f 1)
+    if [ "$command" = "cd" ]; then
+        path=$(pwd)
+        x="amsh:$path> "
+        echo -n "$x"
+    else
+        echo -n "$x"
+    fi
 	read input
 done
